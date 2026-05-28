f62fc9db439d5df6b9475e28fc598e067d540822
Sarthak-2302 <sjsarthakjain2302@gmail.com>
2026-05-28 13:46:01 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react"; 
+function App(){ 
+  const [count,setCount]=useState(0); 
+  const increment = () => { 
+    setCount(count+1); 
+  }; 
+   const decrement = () => { 
+    setCount(count-1); 
+  }; 
+  const reset = () =>{ 
+    setCount(0); 
+  }; 
+  return( 
+    <div className="container"> 
+      <h1>React Counter Application</h1> 
+      <h2 className="count">{count}</h2> 
+      <div className="button"> 
+        <button onClick={increment}>increment (+)</button> 
+        <button onClick={decrement}>decrement (-)</button> 
+        <button onClick={reset}>reset</button> 
+      </div> 
+    </div> 
+ 
+  ); 
+} 
+export default App; 
+import React from "react" 
+import ReactDOM from "react-dom/client"; 
+import App from "./App" 
+import "./App.css" 
+ 
+ReactDOM.createRoot(document.getElementById("root")).render( 
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode> 
+); 
